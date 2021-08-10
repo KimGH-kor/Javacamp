e4f92c5d57577ed18743a40e6bf9ca8d6fe093d5
KimGH-kor <socoolq@naver.com>
2021-08-10 16:33:30 +0900

    30

--- chapter1/src/ch01/HelloJava.java
+++ chapter1/src/ch01/HelloJava.java
@@ -3,7 +3,7 @@ package ch01;
 public class HelloJava {
 
 	public static void main(String[] args) {//main은 실제로 돌리는 곳
-		System.out.println("Hello World");
+		System.out.println("Hello World!!!!");
 
 	}
 
